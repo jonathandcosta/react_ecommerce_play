396cc28ce24ae56b5dacbf041dbeec9a8ba280ea
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-07-10 22:02:00 -0300

    criando o hero e compenentizando Button

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Imagem, Titulo, Precos } from './styles'
+
+import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
+
+const Banner = () => (
+  <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
+    <div className="container">
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <Titulo>Marvel's Spider-Man: Miles Morales PS4 & PS5</Titulo>
+        <Precos>
+          De <span>R$250,00</span> <br />
+          por apenas R$99,90
+        </Precos>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="clique para aproveitar esta oferta"
+      >
+        Aproveitar
+      </Button>
+    </div>
+  </Imagem>
+)
+
+export default Banner
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const cores = {
   branca: '#eee',
@@ -23,10 +23,10 @@ export const GlobalCss = createGlobalStyle`
     padding-top: 40px;
   }
 
-`
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
 
-export const Container = styled.div`
-  max-width: 1024px;
-  width: 100%;
-  margin: 0 auto;
 `
