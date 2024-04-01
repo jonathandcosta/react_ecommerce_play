a8ec443a9ec5144d83aac202322cc241ff2c6294
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-04-01 16:54:27 -0300

    deixando o menu responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
 
-import { HeaderBar, Links, LinkItem, CartButton } from './styles'
+import {
+  HeaderBar,
+  Links,
+  LinkItem,
+  CartButton,
+  Hambuguer,
+  HeaderRow,
+  NavMobile
+} from './styles'
 
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
@@ -19,28 +27,50 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="logo EPLAY" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categories">Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart}>
-        {items.length} - produto(s)
-        <img src={carrinho} alt="carrinho de compras" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hambuguer>
+            <span />
+            <span />
+            <span />
+          </Hambuguer>
+          <Link to="/">
+            <img src={logo} alt="logo EPLAY" />
+          </Link>
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link to="/categories">Categorias</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Novidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Promoções</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton onClick={openCart}>
+          {items.length} <span> - produto(s)</span>
+          <img src={carrinho} alt="carrinho de compras" />
+        </CartButton>
+      </HeaderRow>
+
+      {/* MENU MOBILE */}
+      <NavMobile>
+        <Links>
+          <LinkItem>
+            <Link to="/categories">Categorias</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
